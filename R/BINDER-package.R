bcfbec3d58b3d0b178830732be9bf1f58c6db357
Patrick <patrick.staunton@ucdconnect.ie>
2019-09-03 16:15:35 +0100

    Update R/BINDER-package.R and man/BINDER-package.Rd.

--- R/BINDER-package.R
+++ R/BINDER-package.R
@@ -1,16 +1,9 @@
 #' The 'BINDER' package.
 #'
-#' @description BINDER (BayesIan gene regulatory Networks inferreD via gene coExpression and compaRative genomics) is a hybrid approach, binding coexpression and comparative genomics to infer prokaryotic regulons.
+#' @description BINDER (BayesIan gene regulatory Networks inferreD via gene coExpression and compaRative genomics) is a hybrid approach, integrating coexpression and comparative genomics to infer prokaryotic regulons.
 #'
 #' @docType package
 #' @name BINDER-package
 #' @aliases BINDER
-#' @useDynLib BINDER, .registration = TRUE
-#' @import methods
-#' @import Rcpp
-#' @importFrom rstan sampling
-#'
-#' @references
-#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
 #'
 NULL
